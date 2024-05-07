dca19f6a2d7d0911889983ebf9c9e86343fa67fe
sivajaffey <sivajaffer@gmail.com>
2024-05-07 18:56:24 +0530

    Folder structure changed && audio and background added

---
+++ script.js
@@ -0,0 +1,129 @@
+let Game = 'flames'
+let ckLtr = 0;
+let remainingTxt = '';
+let gNameRm = '';
+let bNameRm = '';
+let flamable = 0;
+const getFlameCount = (char, status) => {
+    if (document.getElementById('flamesTxt') && document.getElementById('flamesTxt')?.style['display'] == 'block') {
+      reset();
+    }
+    let bName = document.getElementById('boyName')?.value?.toLowerCase();
+    let gName = document.getElementById('girlName')?.value?.toLowerCase();
+    if (bName?.trim()?.length > 3 && gName?.trim()?.length > 3) {
+    let curStatus = bName.localeCompare(gName);
+    
+        if (char !== undefined) {
+            let index = gName.indexOf(char)
+            if (index >= 0) {
+                gNameRm = gName.replace(char, "");
+                bNameRm = bName.replace(char, "");
+            }
+        }
+            if (bName.length == ckLtr) {
+                flamable=gNameRm.concat(bNameRm).length;
+                // console.log("Count between :",flamable)
+                flames(flamable, Game.split(''),'');
+                return false
+            }
+
+            if (ckLtr <= status || char == undefined) {
+                ckLtr+=1
+                getFlameCount(bName[ckLtr-1], ckLtr)
+            } else {
+                return false
+            }
+  } else {
+    alert('Boy and Girl Name must contain more than 4 characters')
+  }
+}
+const flames = (Fcount, arr, startChar) => {
+    
+    let getVal = Math.round(Fcount / arr.length);
+    let game = arr.join('');
+    if (arr.length == 1) {
+        // console.log("Final",arr)
+        setFlames(arr[0])
+        return false;
+    }
+
+        for(let i=0; i<arr.length;i++) {
+            game += game
+        }
+
+        if (startChar !== '') {
+            let startIndx=game.indexOf(startChar);
+            game=game.slice(startIndx);
+        }
+        // console.log('Text Aft:', game)
+        let nextChar = game.charAt(Fcount);
+        let rmChar = game.charAt(Fcount-1);
+        if (game.length < Fcount && startChar !== '') {
+            // console.log('startChar',startChar)
+            strikeLtr(startChar)
+            let rmAr = arr.filter(d=>d!=startChar);
+            flames(Fcount, rmAr, nextChar);
+            return false;
+        }
+        if (arr.length == 2 && rmChar == '' && startChar !== '') {
+          rmChar = startChar;
+        }
+        // console.log('arr',arr, 'remove',rmChar)
+        strikeLtr(rmChar)
+        let rmAr = arr.filter(d=>d!=rmChar);
+        Game = rmAr.join('');
+        // console.log('Removed Char', rmChar)
+        // console.log("Remaing", rmAr)
+        // console.log("Start from :",nextChar)
+        if (rmAr.length == 1) {
+            // console.log("Final",rmAr)
+            setFlames(rmAr[rmAr.length-1])
+            return false;
+        }
+        if (nextChar !== '' && (rmAr.length > 1)) {
+            // console.log('--------------------')
+            flames(Fcount, rmAr, nextChar);
+        }
+    // }
+}
+const setFlames = (w) => {
+    document.getElementById('flamesTxt-f').style['display'] = 'none';
+    document.getElementById('flamesTxt-l').style['display'] = 'none';
+    document.getElementById('flamesTxt-a').style['display'] = 'none';
+    document.getElementById('flamesTxt-m').style['display'] = 'none';
+    document.getElementById('flamesTxt-e').style['display'] ='none';
+    document.getElementById('flamesTxt-s').style['display'] = 'none';
+    if (w && document.getElementById('flamesTxt-'+w))
+    {
+      document.getElementById('flamesTxt-'+w).style['display'] = '';
+      document.getElementById('flamesTxt').style['display'] = 'block';
+      document.getElementById('boyName').value = ''
+      document.getElementById('girlName').value = ''
+      let s = document.getElementById('sound-'+w.toString());
+      s?.play();
+    }
+}
+const strikeLtr = (w) => {
+    if (w && document.getElementById(w)) {
+      document.getElementById(w).style.textDecoration = 'line-through'
+    }
+}
+const reset = () => {
+    Game = 'flames'
+    ckLtr = 0;
+    remainingTxt = '';
+    gNameRm = '';
+    bNameRm = '';
+    flamable = 0;
+    document.getElementById('flamesTxt').style.display = 'none';
+    document.getElementById('flame').style.display = '';
+    document.getElementById('f').style.textDecoration = 'none'
+    document.getElementById('l').style.textDecoration = 'none'
+    document.getElementById('a').style.textDecoration = 'none'
+    document.getElementById('m').style.textDecoration = 'none'
+    document.getElementById('e').style.textDecoration = 'none'
+    document.getElementById('s').style.textDecoration = 'none'
+    document.getElementById('boyName').value = ''
+    document.getElementById('girlName').value = ''
+}
+reset();
