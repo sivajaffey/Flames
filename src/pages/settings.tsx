54cffbe2b53c2248d165e1e9dfc4089e8e4b3799
sivajaffey <sivajaffer@gmail.com>
2026-03-09 22:29:41 +0530

    UI issues fixed

--- src/pages/settings.tsx
+++ src/pages/settings.tsx
@@ -105,7 +105,7 @@ const Settings = (props) => {
                 </>
             } />
         }
-        {timer === 0 && <div style={{color: style.color, "display":"flex", "justify-content":"center", "align-items":"center", height: "75%", top:"15%", position: "absolute"}}>
+        {timer === 0 && <div style={{color: style.color, "display":"flex", "justify-content":"center", "align-items":"center", height: "75%", top:"15%", position: "fixed"}}>
             {constant?.lang[state?.lang]?.loading}
         </div>}
         </>
